e2661c927ef94c2a6607bf46bd70a2e23e426c77
Rsam <arsam.hb@gmail.com>
2024-02-08 16:58:53 +0330

    feat: styled-component in QuizResult and styledMUI in SingleQuestion

--- src/features/quiz/QuizResult.jsx
+++ src/features/quiz/QuizResult.jsx
@@ -1,34 +1,44 @@
 import React from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import styled from 'styled-components';
+import ListItemText from '@mui/material/ListItemText';
 
+const StyledListItemText = styled(ListItemText)`
+  color: ${(props) => (props.isCorrect ? 'green' : 'red')};
+  border: 1px solid ${(props) => (props.isCorrect ? 'green' : 'red')};
+  padding: 14px;
+  border-radius: 4px;
+`;
+
+const StyledTypography = styled(Typography)`
+  text-align: center;
+  color: #BF4F74;
+`;
+
+const StyledGrid = styled(Grid)`
+  padding: 18px;
+`;
 
 const QuizResult = ({ quiz }) => {
     return (
-        <>
-            {quiz.map((item, index) =>
-                <span key={index}>{item.userAnswer}</span>
-            )}
-            <Grid item xs={12} md={6}>
-                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-                    RESULT OF YOUR TEST
-                </Typography>
-                <List>
-                    {quiz.map((question) => (
-                        <ListItem>
-                            <ListItemText>
-                                {`for question num.${question.id + 1} your answer was ${question.userAnswer + 1} and ${question.indexOfCorrectAnswer === question.userAnswer ?
-                                    "it was true"
-                                    : "it was false"}`}
-                            </ListItemText>
-                        </ListItem>
-                    ))}
-                </List>
-            </Grid>
-        </>
+        <StyledGrid>
+            <StyledTypography variant="h2">
+                RESULT OF YOUR TEST
+            </StyledTypography>
+            <List>
+                {quiz.map((question) => (
+                    <ListItem key={question.id}>
+                        <StyledListItemText
+                            isCorrect={question.indexOfCorrectAnswer === question.userAnswer}
+                            primary={`For question number "${question.id + 1}-${question.question}" your answer was "${question.userAnswer + 1}-${question.answers[question.userAnswer]}" and ${question.indexOfCorrectAnswer === question.userAnswer ? "it was TRUE" : "it was FALSE"}`}
+                        />
+                    </ListItem>
+                ))}
+            </List>
+        </StyledGrid>
     )
 }
 
--- src/features/quiz/SingleQuestion.jsx
+++ src/features/quiz/SingleQuestion.jsx
@@ -1,27 +1,57 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { styled } from '@mui/system';
+
+// IN ORDER TO HANDLE MUI OVERRIDING ON MY CSS IMPLEMENTED USING
+// STYLED-COMPONENT I USED STYLE FROM MUI/SYSTEM
+// BUT I USED STYLED-COMPONENT IN THE QuizResult COMPONENT AS YOU CAN SEE
+const StyledFormControl = styled(FormControl)({
+    backgroundColor: 'aliceblue',
+    padding: '2rem',
+    width: '100vw',
+});
+
+const StyledFormLabel = styled(FormLabel)({
+    color: '#1976DB',
+    fontSize: "24px",
+    fontWeight: 700,
+});
+
+const StyledFormControlLabel = styled(FormControlLabel)({
+    margin: '1rem',
+    padding: '1rem',
+    maxWidth: '80%',
+    border: "1px solid black",
+    '&:hover': {
+        backgroundColor: 'rgba(0, 0, 0, 0.1)', 
+        cursor: 'pointer', 
+    },
+})
+
+const StyledRadio = styled(Radio)({
+    display: 'none'
+})
 
 
 const SingleQuestion = ({ question, selectAnswer }) => {
-
     return (
-        <FormControl>
-            <FormLabel id={question.question}>{question.question}</FormLabel>
+        <StyledFormControl>
+            <StyledFormLabel id={question.question}>{question.question}</StyledFormLabel>
             <RadioGroup
                 aria-labelledby={question.question}
                 defaultValue={undefined}
-                name={`question number ${question.id}`}
+                name={`question number ${question.id} `}
             >
                 {question.answers.map((answer, index) =>
-                    <FormControlLabel
+                    <StyledFormControlLabel
                         key={index}
                         value={index}
-                        control={<Radio
-                            checked={false}
+                        control={<StyledRadio
+                            checked={question.userAnswer === index}
                             onChange={() => {
                                 selectAnswer(index)
                             }}
@@ -29,7 +59,7 @@ const SingleQuestion = ({ question, selectAnswer }) => {
                         label={answer} />
                 )}
             </RadioGroup>
-        </FormControl>
+        </StyledFormControl>
     )
 }
 
